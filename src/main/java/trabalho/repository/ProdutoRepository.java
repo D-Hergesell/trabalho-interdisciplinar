ccf25125c7a799a0bc90f6f8ef607d15c3ee4b83
Diego Hergesell <diegohergesell2@gmail.com>
2025-10-24 17:13:46 -0300

    Ajustes: - Criado método do ProdutoRepository.java

--- src/main/java/trabalho/repository/ProdutoRepository.java
+++ src/main/java/trabalho/repository/ProdutoRepository.java
@@ -3,8 +3,12 @@ package trabalho.repository;
 import trabalho.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+    List<Produto> findByFornecedorId(UUID fornecedorId);
+    List<Produto> findByCategoriaId(UUID categoriaId);
 }
