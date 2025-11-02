e5d956481ef9daf61ebe1cfa3c12d5f8bd354eaf
Diego Hergesell <diegohergesell2@gmail.com>
2025-11-01 21:13:08 -0300

    Adicionado o Repository de CondicoesEstadoRepository

--- src/main/java/trabalho/repository/CondicoesEstadoRepository.java
+++ src/main/java/trabalho/repository/CondicoesEstadoRepository.java
@@ -3,8 +3,10 @@ package trabalho.repository;
 import trabalho.entities.CondicoesEstado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CondicoesEstadoRepository extends JpaRepository<CondicoesEstado, UUID> {
+    Optional<CondicoesEstado> findByFornecedorIdAndEstado(UUID fornecedorId, String estado);
 }
