4abfe62beedf346a0be407bfd7a4b8cda250d5a0
Diego Hergesell <diegohergesell2@gmail.com>
2025-11-18 16:54:28 -0300

    Correção no LojaRepository.java

--- src/main/java/trabalho/repository/LojaRepository.java
+++ src/main/java/trabalho/repository/LojaRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface LojaRepository extends JpaRepository<Loja, UUID>{
-    List<Loja> findByNomeContainingIgnoreCase(String nome);
+    List<Loja> findByNomeFantasiaContainingIgnoreCase(String nomeFantasia);
 }
