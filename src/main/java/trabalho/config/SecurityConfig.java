c0b11384329ad10400a78a0d0776a0f9fc6d5426
Diego Hergesell <diegohergesell2@gmail.com>
2025-11-18 16:53:35 -0300

    Criação do SecurityConfig.java com filterChain para permitir testes.

---
+++ src/main/java/trabalho/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package trabalho.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desativa CSRF para API
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // LIBERA TUDO
+                );
+
+        return http.build();
+    }
+
+}
